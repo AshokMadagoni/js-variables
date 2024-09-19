b2b4e70a1fd614cee1758e940a2e108084b6472e
AshokMadagoni <madagoniashok1@gmail.com>
2024-09-19 19:43:42 +0530

    added

---
+++ practice.js
@@ -0,0 +1,21 @@
+var a=10;
+let b=20;
+const c=30;
+console.log(a);
+console.log(b);
+console.log(c);
+
+{
+    var d=40;
+    let e=50;
+    const f=60;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+    
+    console.log(d);
+    console.log(e);
+
+}
+console.log(d);
+console.log(b);
